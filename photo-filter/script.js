617d30295eded16488af9e5ad7a0e4a68b9b4f60
abizyana <abi3yana@gmail.com>
2021-04-22 23:22:47 +0200

    feat: add reset styles

--- photo-filter/script.js
+++ photo-filter/script.js
@@ -12,3 +12,35 @@ function handleUpdate() {
     input.value + suffix
   );
 }
+
+const resetBtn = document.querySelector(".btn-reset");
+resetBtn.addEventListener("click", reset);
+
+function reset() {
+  inputs.forEach(
+    (input) =>
+      (input.querySelector("input").value = input
+        .querySelector("input")
+        .getAttribute("value"))
+  );
+  inputs.forEach(
+    (input) =>
+      (input.querySelector("output").value = input
+        .querySelector("input")
+        .getAttribute("value"))
+  );
+  document.documentElement.style.setProperty("--blur", "0px");
+  document.documentElement.style.setProperty("--invert", "0%");
+  document.documentElement.style.setProperty("--sepia", "0%");
+  document.documentElement.style.setProperty("--saturate", "100%");
+  document.documentElement.style.setProperty("--hue", "0deg");
+}
+
+let fullScreen = document.querySelector(".fullscreen");
+fullScreen.addEventListener("click", () => {
+  if (!document.fullscreenElement) {
+    document.body.requestFullscreen();
+  } else {
+    document.exitFullscreen();
+  }
+});
