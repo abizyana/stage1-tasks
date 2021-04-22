2e89c9f5e6f906e550b9e59f6f4894a1ac76ba9a
abizyana <abi3yana@gmail.com>
2021-04-22 23:47:36 +0200

    feat: add next btn for dummy hour

--- photo-filter/script.js
+++ photo-filter/script.js
@@ -36,6 +36,46 @@ function reset() {
   document.documentElement.style.setProperty("--hue", "0deg");
 }
 
+const baseUrl =
+  "https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/day/";
+const images = [
+  "01.jpg",
+  "02.jpg",
+  "03.jpg",
+  "05.jpg",
+  "06.jpg",
+  "07.jpg",
+  "08.jpg",
+  "09.jpg",
+  "10.jpg",
+  "11.jpg",
+  "12.jpg",
+  "13.jpg",
+  "14.jpg",
+  "15.jpg",
+  "16.jpg",
+  "17.jpg",
+  "18.jpg",
+  "19.jpg",
+  "20.jpg",
+];
+
+let img = document.querySelector("img");
+let nextImageBtn = document.querySelector(".btn-next");
+nextImageBtn.addEventListener("click", nextImage);
+
+let i = 0;
+function nextImage() {
+  const loadedImage = new Image();
+  index = i % images.length;
+  loadedImage.src = baseUrl + images[index];
+
+  loadedImage.onload = () => {
+    img.setAttribute("src", baseUrl + images[index]);
+  };
+  i++;
+}
+
 let fullScreen = document.querySelector(".fullscreen");
 fullScreen.addEventListener("click", () => {
   if (!document.fullscreenElement) {
